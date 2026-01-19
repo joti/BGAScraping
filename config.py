c11904537729538cd4a4d7566e16a77db5bc3142
joti <tihonjozsef@gmail.com>
2026-01-19 23:09:56 +0100

    headless and no-sandbox options

--- config.py
+++ config.py
@@ -20,9 +20,21 @@ output_dir = PATHS['output_dir']
 input_dir = PATHS['input_dir']
 db_path = PATHS['db_path']
 
-service = Service()
-options = webdriver.ChromeOptions()
-options.binary_location = PATHS['chrome_path']
-options.add_argument("--log-level=1")
-DRIVER = webdriver.Chrome(service=service, options=options)
-WAIT = WebDriverWait(DRIVER, 15)
+def create_driver( headless = False, 
+                   no_sandbox = False ):
+    service = Service()
+    options = webdriver.ChromeOptions()
+    options.binary_location = PATHS['chrome_path']
+    options.add_argument("--log-level=1")
+
+    if headless:
+        options.add_argument("--headless=new")
+        options.add_argument("--window-size=1920,1080")
+    if no_sandbox:    
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+
+    driver = webdriver.Chrome(service=service, options=options)
+    wait = WebDriverWait(driver, 15)
+
+    return driver, wait
